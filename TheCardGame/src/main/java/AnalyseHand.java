581c5089265db0e3d484cd210745d4e6f1b9a743
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-24 21:30:08 +0100

    feat(AnalyseHand): add checkStraight method.

--- TheCardGame/src/main/java/AnalyseHand.java
+++ TheCardGame/src/main/java/AnalyseHand.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 
 public class AnalyseHand {
@@ -23,4 +24,16 @@ public class AnalyseHand {
     }
     return true;
   }
+
+  public static int checkStraight(List<PlayingCard> hand) {
+    hand.sort((card1, card2) -> card1.getFace() - card2.getFace());  // Sorter etter face
+
+    for (int i = 0; i < hand.size() - 1; i++) {
+      if (hand.get(i).getFace() + 1 != hand.get(i + 1).getFace()) {
+        return 0;  // Ikke straight
+      }
+    }
+    return 4;  // Straight gir 4 poeng
+  }
+
 }

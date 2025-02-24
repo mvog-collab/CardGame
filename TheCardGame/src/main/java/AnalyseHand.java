606cb0dfd9ed2f2307537ea163d899017c7e531c
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-24 21:18:06 +0100

    feat(AnalyseHand): create isFlush method

---
+++ TheCardGame/src/main/java/AnalyseHand.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class AnalyseHand {
+
+    public boolean isFlush(List<PlayingCard> hand) {
+      if (hand == null || hand.isEmpty()) {
+        return false;
+      }
+      char firstSuit = hand.get(0).getSuit();
+
+      for (PlayingCard card : hand) {
+        if (card.getSuit() != firstSuit) {
+          return false;
+        }
+      }
+      return true;
+  }
+}

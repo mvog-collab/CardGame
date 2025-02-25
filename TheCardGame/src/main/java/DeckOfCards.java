475ab751cecb909d397d6f8ab874f1df34e7fcfb
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-25 17:46:31 +0100

    debug(DeckOfCards): fix reshuffel when cards are empty.

--- TheCardGame/src/main/java/DeckOfCards.java
+++ TheCardGame/src/main/java/DeckOfCards.java
@@ -2,27 +2,40 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import javafx.scene.text.Text;
 
 public class DeckOfCards {
   private ArrayList<PlayingCard> deck;
   private final char[] suits = { 'H', 'S', 'C', 'D'}; //  hjerter, spar, kløver og ruter
+  Random rand = new Random();
+
 
   public DeckOfCards() {
-    this.deck = new ArrayList<PlayingCard>();
+    initializeDeck();
+  }
+
+  public void initializeDeck() {
+    deck = new ArrayList<>();
     for (char suit : suits) {
-      for (int rank = 1; rank < 13; rank++) {
+      for (int rank = 1; rank <= 13; rank++) {
         deck.add(new PlayingCard(suit, rank));
       }
     }
+    shuffleDeck();
+  }
+
+
+  private void shuffleDeck() {
+    Collections.shuffle(deck, rand);
   }
 
   public List<PlayingCard> dealHand(int n) {
-    if (n < 1 || n > deck.size()) {
-      throw new IllegalArgumentException("Number of cards to deal must be between 1 and 52");
+    if (n < 1 ) {
+      throw new IllegalArgumentException("Number of cards to deal must be at least 1");
     }
-
-    Random rand = new Random();
-    Collections.shuffle(deck, rand);  //blander kortene
+    if (deck.size() < n) {
+      initializeDeck();
+    }
 
     List<PlayingCard> hand = new ArrayList<PlayingCard>();
     for (int i = 0; i < n; i++) {
@@ -30,6 +43,4 @@ public class DeckOfCards {
     }
     return hand;
   }
-
-
 }
